561182325b86d315bf54f9135a7fd8d61fcd94e3
summitbajaj <95735368+summitbajaj@users.noreply.github.com>
2025-02-16 17:16:06 +0800

    correct db folder setup for base

--- server/database/alembic/env.py
+++ server/database/alembic/env.py
@@ -3,9 +3,10 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
-import models 
-from database import Base
+from database.models import Base, Topic, Problem, TestCase, Example
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
