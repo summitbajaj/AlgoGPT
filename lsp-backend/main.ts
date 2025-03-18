5261ade6bb9dbb57b73cb83d4ae06634ac2a9cd7
summitbajaj <95735368+summitbajaj@users.noreply.github.com>
2025-03-18 20:28:41 +0800

    disable logging of messages in Python server for cleaner output

--- lsp-backend/main.ts
+++ lsp-backend/main.ts
@@ -36,6 +36,6 @@ export const runPythonServer = (baseDir: string, relativeDir: string) => {
                 }
             }
         },
-        logMessages: true
+        logMessages: false
     });
 };
